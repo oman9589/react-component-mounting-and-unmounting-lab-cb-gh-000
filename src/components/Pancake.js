808fab0f4a2baefde6ae6f05dbddee6143729aa2
Oliver Spring <oliver9589@gmail.com>
2017-09-23 22:46:41 +0000

    automatically backed up by learn

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -12,7 +12,7 @@ class Pancake extends React.Component {
   }
 
   componentDidMount = () => {
-
+    this.startInterval();
   }
   // TODO: create a componentWillUnmount() which will clear the interval
 
